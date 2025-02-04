812555575d748d2a1344982350940ddee307e0ab
anshbegmirza <anshbegmirza@gmail.com>
2025-02-04 15:12:09 +0530

    Completed submitted for review

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-2';
+
+  @ViewChild('boxContainer', { static: false }) boxContainer!: ElementRef;
+
+  changeTextColor(event: any) {
+    this.boxContainer.nativeElement.style.color = event?.target.value;
+  }
+
+  changeBackgroundColor(event: any) {
+    this.boxContainer.nativeElement.style.backgroundColor = event?.target.value;
+  }
 }
